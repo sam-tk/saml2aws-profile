a7bc41eb2e8ad678c7826c14f62e22e616b4d269
Masahiro Yoshitake <masahiro@3hands.co.jp>
2022-03-29 23:52:02 +0900

    sectionを名前ソートするようにして、一部のunwrap()処理を書き換えた

--- src/main.rs
+++ src/main.rs
@@ -9,17 +9,37 @@ fn main() {
     table.set_format(*format::consts::FORMAT_NO_BORDER_LINE_SEPARATOR);
     table.set_titles(row!["AWS_PROFILE","⏰","Expire Time","AWS Account","Rolename"]);
 
+    // TODO: 環境変数をチェックする env "AWS_SHARED_CREDENTIALS_FILE"
     let mut cred_file = dirs::home_dir().unwrap();
     cred_file.push(".aws/credentials") ; 
-    let cred = Ini::load_from_file(cred_file).unwrap();
 
-    for (sec, prop) in cred.iter() {
+    let cred = match Ini::load_from_file(cred_file) {
+        Ok(ini) => ini,
+        Err(error) => panic!("{}",error)
+    };
+
+    // iniのセクションをソートしておく
+    // TODO: defaultをどうする?
+    let mut sections = vec![];
+    for (sec, _prop) in cred.iter() {
+        sections.push(sec);
+    }
+    sections.sort();
+
+    for sec in sections {
+        let prop = match cred.section(sec.to_owned()) {
+            Some(p) => p,
+            None => panic!()
+        };
         let acc ;
         let rolename ;
         let expires ;
 
         if prop.contains_key("x_principal_arn") {
-            let arn = prop.get("x_principal_arn").unwrap();
+            let arn = match prop.get("x_principal_arn") {
+                Some(v) => v,
+                None => panic!()
+            };
             let valid ;
             acc = arn.split(":").nth(4).unwrap();
             rolename = arn.split("/").nth(1).unwrap();
@@ -37,6 +57,6 @@ fn main() {
 
     }
     table.printstd();
-    
+    // TODO: exit codeを考慮しなくてもよい?
 }
 
